f094c1045704fff89cdf355df7baee8729c3b3dd
Sebastian Kürten <sebastian@topobyte.de>
2019-05-31 09:31:38 +0200

    Add a test that explores a git repo's commits and files

---
+++ core/src/test/java/de/topobyte/git/utils/TestCommitFiles.java
@@ -0,0 +1,93 @@
+package de.topobyte.git.utils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.jgit.api.DiffCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import de.topobyte.system.utils.SystemPaths;
+
+public class TestCommitFiles
+{
+
+	public static void main(String[] args)
+			throws IOException, NoHeadException, GitAPIException
+	{
+		System.out.println("Test");
+		Path repoPath = SystemPaths.HOME.resolve("github/topobyte/jts-utils");
+
+		Git git = Git.open(repoPath.toFile());
+		Repository repository = git.getRepository();
+
+		Iterable<RevCommit> commits = git.log().call();
+
+		RevCommit last = null;
+
+		for (RevCommit commit : commits) {
+			if (last == null) {
+				last = commit;
+				continue;
+			}
+
+			diff(git, last, commit);
+
+			last = commit;
+		}
+
+		print(last);
+		RevTree tree = last.getTree();
+		try (TreeWalk treeWalk = new TreeWalk(repository)) {
+			treeWalk.reset(tree);
+			while (treeWalk.next()) {
+				String path = treeWalk.getPathString();
+				System.out.println(path);
+			}
+		}
+
+		git.close();
+	}
+
+	private static void diff(Git git, RevCommit commit, RevCommit previous)
+			throws IncorrectObjectTypeException, IOException, GitAPIException
+	{
+		Repository repository = git.getRepository();
+
+		print(commit);
+
+		DiffCommand diff = git.diff();
+		diff.setOldTree(new CanonicalTreeParser(null,
+				repository.newObjectReader(), previous.getTree()));
+		diff.setNewTree(new CanonicalTreeParser(null,
+				repository.newObjectReader(), commit.getTree()));
+		List<DiffEntry> changes = diff.call();
+		System.out.println("changes: " + changes.size());
+		for (DiffEntry change : changes) {
+			ChangeType type = change.getChangeType();
+			System.out.println(type + " " + change.getOldPath() + " "
+					+ change.getNewPath());
+		}
+	}
+
+	private static void print(RevCommit commit)
+	{
+		System.out.println(String.format("COMMIT %s", commit.getName()));
+		Date time = commit.getCommitterIdent().getWhen();
+		System.out.println(time);
+		String message = commit.getFullMessage();
+		System.out.println(message);
+	}
+
+}
